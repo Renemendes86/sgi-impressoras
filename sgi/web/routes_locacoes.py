810f7c7af83d71ad2f70f78322cbae775177cc33
Renemendes86 <renemfto@hotmail.com>
2026-07-06 10:01:45 -0300

    Update routes_locacoes.py

--- sgi/web/routes_locacoes.py
+++ sgi/web/routes_locacoes.py
@@ -11,7 +11,6 @@ from flask import (
 )
 
 from datetime import datetime, date
-from sgi.core.services.historico import registrar_historico
 from sgi.core.db import conectar
 from sgi.core.permissions import (
     login_required,
@@ -785,3 +784,5 @@ def configurar_rotas_locacoes(app):
             conn.close()
 
         return redirect(f"/locacoes/{cliente_id}")
+
+
